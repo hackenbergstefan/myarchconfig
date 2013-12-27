22fd66e70957cee71a4798028e356b9f1e0ab53f
zero-git <hackenberg.stefan@t-online.de>
2013-12-27 11:34:41 +0100

    update

--- redate.sh
+++ redate.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 echo "copy"
 echo
-cp -v ~/myarchconfig/Xresources      ~/.Xresources           
-cp -v ~/myarchconfig/aliasrc         ~/.aliasrc              
-cp -v ~/myarchconfig/bash_profile    ~/.bash_profile         
-cp -v ~/myarchconfig/bashrc          ~/.bashrc               
-cp -v ~/myarchconfig/tmux.conf       ~/.tmux.conf            
-cp -v ~/myarchconfig/vimencrypt      ~/.vimencrypt           
-cp -v ~/myarchconfig/vimrc           ~/.vimrc                
-cp -v ~/myarchconfig/xinitrc         ~/.xinitrc              
-cp -v ~/myarchconfig/                ~/.xmonad/xmobarrc      
-cp -v ~/myarchconfig/                ~/.xmonad/xmonad.hs     
-cp -v ~/myarchconfig/zshrc.local     ~/.zshrc.local          
-cp -v ~/myarchconfig/bin/            ~/bin/disp-controll     
-cp -v ~/myarchconfig/bin/            ~/bin/myfsize.sh        
+cp -v --parents ~/myarchconfig/Xresources      ~/.Xresources           
+cp -v --parents ~/myarchconfig/aliasrc         ~/.aliasrc              
+cp -v --parents ~/myarchconfig/bash_profile    ~/.bash_profile         
+cp -v --parents ~/myarchconfig/bashrc          ~/.bashrc               
+cp -v --parents ~/myarchconfig/tmux.conf       ~/.tmux.conf            
+cp -v --parents ~/myarchconfig/vimencrypt      ~/.vimencrypt           
+cp -v --parents ~/myarchconfig/vimrc           ~/.vimrc                
+cp -v --parents ~/myarchconfig/xinitrc         ~/.xinitrc              
+cp -v --parents ~/myarchconfig/                ~/.xmonad/xmobarrc      
+cp -v --parents ~/myarchconfig/                ~/.xmonad/xmonad.hs     
+cp -v --parents ~/myarchconfig/zshrc.local     ~/.zshrc.local          
+cp -v --parents ~/myarchconfig/bin/            ~/bin/disp-controll     
+cp -v --parents ~/myarchconfig/bin/            ~/bin/myfsize.sh        
 
