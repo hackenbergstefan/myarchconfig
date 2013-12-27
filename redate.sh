85fa1203103b19d752c1831eb2fb9bb8ba12a7f7
zero-git <hackenberg.stefan@t-online.de>
2013-12-27 11:06:16 +0100

    update

---
+++ redate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "copy"
+echo
+cp -v ~/myarchconfig/Xresources      ~/.Xresources           
+cp -v ~/myarchconfig/aliasrc         ~/.aliasrc              
+cp -v ~/myarchconfig/bash_profile    ~/.bash_profile         
+cp -v ~/myarchconfig/bashrc          ~/.bashrc               
+cp -v ~/myarchconfig/tmux.conf       ~/.tmux.conf            
+cp -v ~/myarchconfig/vimencrypt      ~/.vimencrypt           
+cp -v ~/myarchconfig/vimrc           ~/.vimrc                
+cp -v ~/myarchconfig/xinitrc         ~/.xinitrc              
+cp -v ~/myarchconfig/                ~/.xmonad/xmobarrc      
+cp -v ~/myarchconfig/                ~/.xmonad/xmonad.hs     
+cp -v ~/myarchconfig/zshrc.local     ~/.zshrc.local          
+cp -v ~/myarchconfig/bin/            ~/bin/disp-controll     
+cp -v ~/myarchconfig/bin/            ~/bin/myfsize.sh        
+
