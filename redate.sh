a9112d6d9ca52d358b5741644b43a7023f179f7a
zero-git <hackenberg.stefan@t-online.de>
2013-12-27 11:43:22 +0100

    update

--- redate.sh
+++ redate.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+echo "creating directories"
+echo
+mkdir ~/bin
+mkdir ~/.xmonad
+
 echo "copy"
 echo
-cp -v --parents ~/myarchconfig/Xresources      ~/.Xresources           
-cp -v --parents ~/myarchconfig/aliasrc         ~/.aliasrc              
-cp -v --parents ~/myarchconfig/bash_profile    ~/.bash_profile         
-cp -v --parents ~/myarchconfig/bashrc          ~/.bashrc               
-cp -v --parents ~/myarchconfig/tmux.conf       ~/.tmux.conf            
-cp -v --parents ~/myarchconfig/vimencrypt      ~/.vimencrypt           
-cp -v --parents ~/myarchconfig/vimrc           ~/.vimrc                
-cp -v --parents ~/myarchconfig/xinitrc         ~/.xinitrc              
-cp -v --parents ~/myarchconfig/                ~/.xmonad/xmobarrc      
-cp -v --parents ~/myarchconfig/                ~/.xmonad/xmonad.hs     
-cp -v --parents ~/myarchconfig/zshrc.local     ~/.zshrc.local          
-cp -v --parents ~/myarchconfig/bin/            ~/bin/disp-controll     
-cp -v --parents ~/myarchconfig/bin/            ~/bin/myfsize.sh        
+cp -v  ~/myarchconfig/Xresources                 ~/.Xresources           
+cp -v  ~/myarchconfig/aliasrc                    ~/.aliasrc              
+cp -v  ~/myarchconfig/bash_profile               ~/.bash_profile         
+cp -v  ~/myarchconfig/bashrc                     ~/.bashrc               
+cp -v  ~/myarchconfig/tmux.conf                  ~/.tmux.conf            
+cp -v  ~/myarchconfig/vimencrypt                 ~/.vimencrypt           
+cp -v  ~/myarchconfig/vimrc                      ~/.vimrc                
+cp -v  ~/myarchconfig/xinitrc                    ~/.xinitrc              
+cp -v  ~/myarchconfig/xmobarrc                   ~/.xmonad/xmobarrc      
+cp -v  ~/myarchconfig/xmonad.hs                  ~/.xmonad/xmonad.hs     
+cp -v  ~/myarchconfig/zshrc.local                ~/.zshrc.local          
+cp -v  ~/myarchconfig/bin/disp-controll          ~/bin/disp-controll     
+cp -v  ~/myarchconfig/bin/myfsize.sh             ~/bin/myfsize.sh        
 
